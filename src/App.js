a91c808f48584fd615c346b4047e0f504086d9ec
Hiếu Trần <you@example.com>
2023-06-03 13:01:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/header';
+import Show from './components/Show_product';
+import Cart from './Cart';
+import Footer from './components/footer';
+import Admin from './components/Admin';
+import { Routes,Route} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+         <Header/>
+         <Routes>
+         <Route path='/' element={<Show/>}/>
+            <Route path='/Cart' element={<Cart/>}/>
+            <Route path='/Admin' element={<Admin/>}/>;
+         </Routes>
+        
     </div>
+     
   );
 }
 
